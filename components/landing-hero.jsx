62a565f42f45ace8920aa6ca705128bd2a8435ac
aviral <avi14320@gmail.com>
2023-10-24 12:54:47 +0530

    fixed bugs in landing-hero

--- components/landing-hero.jsx
+++ components/landing-hero.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import TypewriterComponent from "typewriter-effect";
+// import TypewriterComponent from "typewriter-effect";
+import Typewriter from "typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 
@@ -15,7 +16,7 @@ export const LandingHero = () => {
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          <TypewriterComponent
+          {/* <TypewriterComponent
             options={{
               strings: [
                 "Chatbot.",
@@ -34,6 +35,22 @@ export const LandingHero = () => {
               onStringTyped: (arrayPos: any, self: any) => {
                 self.el.classList.remove("typewriter-blur");
               },
+            }}
+          /> */}
+
+          <Typewriter
+            options={{
+              strings: [
+                "Chatbot.",
+                "Photo Generation.",
+                "Blog Writing.",
+                "Mail Writing.",
+              ],
+              autoStart: true,
+              loop: true,
+              pauseFor: 5000,
+              deleteSpeed: 10,
+              delay: 10,
             }}
           />
         </div>
