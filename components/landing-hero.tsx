209a2c77c1d18fbc7de7b5502add052074da98aa
aviral <avi14320@gmail.com>
2023-10-24 13:35:41 +0530

    updated files

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-// import TypewriterComponent from "typewriter-effect";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
+import TypewriterComponent from "typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 
@@ -16,7 +16,7 @@ export const LandingHero = () => {
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          {/* <TypewriterComponent
+          <TypewriterComponent
             options={{
               strings: [
                 "Chatbot.",
@@ -28,31 +28,31 @@ export const LandingHero = () => {
               loop: true,
               deleteSpeed: 10,
               delay: 10,
-              pauseFor: 5000,
-              preStringTyped: (arrayPos: any, self: any) => {
-                self.el.classList.add("typewriter-blur");
-              },
-              onStringTyped: (arrayPos: any, self: any) => {
-                self.el.classList.remove("typewriter-blur");
-              },
-            }}
-          /> */}
-
-          <Typewriter
-            options={{
-              strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Blog Writing.",
-                "Mail Writing.",
-              ],
-              autoStart: true,
-              loop: true,
-              pauseFor: 5000,
-              deleteSpeed: 10,
-              delay: 10,
+              // pauseFor: 5000,
+              // preStringTyped: (arrayPos: any, self: any) => {
+              //   self.el.classList.add("typewriter-blur");
+              // },
+              // onStringTyped: (arrayPos: any, self: any) => {
+              //   self.el.classList.remove("typewriter-blur");
+              // },
             }}
           />
+
+          {/* <Typewriter
+            options={{
+              strings: [
+                "Chatbot.",
+                "Photo Generation.",
+                "Blog Writing.",
+                "Mail Writing.",
+              ],
+              autoStart: true,
+              loop: true,
+              pauseFor: 5000,
+              deleteSpeed: 10,
+              delay: 10,
+            }}
+          /> */}
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
@@ -62,13 +62,11 @@ export const LandingHero = () => {
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button
             variant="premium"
-            // radius="full"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
           >
             Start Generating For Free
           </Button>
         </Link>
-        {/* <CircularProgress aria-label="Loading..." /> */}
       </div>
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
