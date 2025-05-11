f3b83a24cfc2378795c4ba115b95f40feaa429e9
Aviral <36530173+aviral17@users.noreply.github.com>
2025-05-11 23:37:59 +0530

    Update landing-content.tsx

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -33,7 +33,7 @@ const testimonials = [
   },
 ];
 
-// updated as per latest changes
+// update as per latest changes
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
